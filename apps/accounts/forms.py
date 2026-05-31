a2eb765ef20553fe1443d82f55875a9234a98e08
Mamun <beparimamun708@gmail.com>
2026-05-31 18:21:24 +0600

    Added forms.py

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -1,1 +1,36 @@
 
+from django import forms
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class RegisterForm(forms.ModelForm):
+   password = forms.CharField(
+      widget=forms.PasswordInput
+   )
+   class Meta:
+      model = User
+      fields = [
+         'username',
+         'email',
+         'password'
+      ]
+   
+   def clean_email(self):
+      email = self.cleaned_data['email']
+      if User.objects.filter(email=email).exists():
+         raise forms.ValidationError(
+            'Email already exists'
+         )
+      return email
+   
+   def save(self, commit=True):
+      user = super().save(commit=False)
+      user.set_password(
+         self.cleaned_data['password']
+      )
+      if commit:
+         user.save()
+      
+      return user 
+   
