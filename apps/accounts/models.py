0f1971e3c74eba2c6360e7acec4d550e79efa1de
Mamun <beparimamun708@gmail.com>
2026-05-31 14:56:38 +0600

    Added Profile model and create singnals

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -9,3 +9,27 @@ class User(AbstractUser):
 
    def __str__(self):
       return self.email
+   
+
+class Profile(models.Model):
+   user = models.OneToOneField(
+      User,
+      on_delete=models.CASCADE,
+      related_name='profile'
+   )
+   full_name = models.CharField(max_length=255)
+   profile_image = models.ImageField(
+      upload_to='profiles/',
+      blank=True,
+      null=True
+   )
+   address = models.TextField(
+      blank=True,
+      null=True
+   )
+   created_at = models.DateTimeField(
+      auto_now_add=True
+   )
+
+   def __str__(self):
+      return self.user.email 
