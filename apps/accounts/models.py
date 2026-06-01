d1a5752f340f0b1b64b6919a21237bebaa8125b8
Mamun <beparimamun708@gmail.com>
2026-06-01 13:16:50 +0600

    Added ROLE FIELD

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -2,8 +2,21 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+   ROLE_CHOICES = (
+      ('customer', 'Customer'),
+      ('seller', 'Seller'),
+      ('admin', 'Admin'),
+   )
+
    email = models.EmailField(unique=True)
    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+   role = models.CharField(
+      max_length=20,
+      choices=ROLE_CHOICES,
+      default='customer'
+   )
+   
    USERNAME_FIELD = 'email'
    REQUIRED_FIELDS = ['username']
 
